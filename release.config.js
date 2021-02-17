e84d86b16bf8afa59d7a303db28b6fc9f712fc60
Mike Shaw <mike.shaw@engineering.digital.dwp.gov.uk>
2021-02-17 16:21:14 +0000

    feat: use push options to skip CI when creating a release
    Signed-off-by: Mike Shaw <mike.shaw@engineering.digital.dwp.gov.uk>

--- release.config.js
+++ release.config.js
@@ -11,6 +11,7 @@ module.exports = {
     "@semantic-release/changelog",
     "@semantic-release/gitlab",
     ["@semantic-release/git", {
+      message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
       assets: [
         'CHANGELOG.md',
       ],
